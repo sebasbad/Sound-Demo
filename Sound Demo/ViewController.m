11c5a23741e773cb309577d6eebc5461cdd64609
Sebastián Badea <sebastian.badea@infojobs.net>
2016-05-19 07:19:28 +0200

    Added buttons actions definition to the main view controller

--- Sound Demo/ViewController.m
+++ Sound Demo/ViewController.m
@@ -19,9 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playSoundA:(id)sender {
+}
+
+- (IBAction)playSoundB:(id)sender {
 }
 
 @end
