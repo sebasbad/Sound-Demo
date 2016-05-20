1cecc47d6a9e2910b4fbb1c8117b96eac6e310e6
Sebastián Badea <sebastian.badea@infojobs.net>
2016-05-20 07:37:29 +0200

    Created audio services sound system id for each sound

--- Sound Demo/ViewController.m
+++ Sound Demo/ViewController.m
@@ -14,6 +14,9 @@
 @property (assign, nonatomic) SystemSoundID beepHightone;
 @property (assign, nonatomic) SystemSoundID beepAttention;
 
+@property (assign, nonatomic) BOOL beepHightoneGood;
+@property (assign, nonatomic) BOOL beepAttentionGood;
+
 @end
 
 @implementation ViewController
@@ -28,6 +31,33 @@
     
     NSString *beepAttentionPath = [[NSBundle mainBundle] pathForResource:@"beep-attention" ofType:@"aif"];
     NSURL *beepAttentionUrl = [NSURL fileURLWithPath:beepAttentionPath];
+    
+    // Archaic C code
+    // __bridge = C-level cast
+    // Tells ARC to stop taking notice of casted object
+    // Casting -> Don't generate ARC meta data
+    OSStatus beepHightoneOSStatus = AudioServicesCreateSystemSoundID((__bridge CFURLRef)beepHightoneUrl, &_beepHightone);
+    
+    if (kAudioServicesNoError == beepHightoneOSStatus) {
+        self.beepHightoneGood = YES;
+    } else {
+        self.beepHightoneGood = NO;
+        
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Couldn't load beep-hightone" message:@"beep-hightone problem" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+    
+    OSStatus beepAttentionOSStatus = AudioServicesCreateSystemSoundID((__bridge CFURLRef)beepHightoneUrl, &_beepAttention);
+    
+    if (kAudioServicesNoError == beepAttentionOSStatus) {
+        self.beepAttentionGood = YES;
+    } else {
+        self.beepAttentionGood = NO;
+        
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Couldn't load beep-attention" message:@"beep-attention problem" preferredStyle:UIAlertControllerStyleAlert];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+    
 }
 
 - (IBAction)playSoundA:(id)sender {
